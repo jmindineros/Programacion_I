dc11b9d00c5c721535988b5c72fa78db4356fb75
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-09-30 21:38:55 +0000

    terminado

---
+++ Ejerciciodowhile.java
@@ -0,0 +1,28 @@
+public class Ejerciciodowhile {
+    public static void main(String[] args) {
+
+        // while
+        int j = 0;
+        String cad = "programacion 1";
+        do {
+
+            System.out.println(cad);
+            j++;
+        } while (j > 5);
+        System.out.println("*******************************");
+        int k = 1;
+        while (k < 5) {
+            System.out.println(cad);
+            k++;
+        }
+        // for
+
+        System.out.println("*******************************");
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println(cad);
+        }
+
+    }
+
+}
