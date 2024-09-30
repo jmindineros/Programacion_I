d32528b1c17cc5336960416e0b50f28636736364
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-09-30 20:41:44 +0000

    ejercicio terminado 56

--- CLASE30/09/24/Ejercicio56.java
+++ CLASE30/09/24/Ejercicio56.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Ejercicio56{
     public static void main (String[] args){
+        //notas de los estudiantes
+            Scanner sc = new Scanner(System.in);
+            double nota= 0;
+            int aprobado=0;
+            int reprobado=0;
+            System.out.print("digite la cantidad de estudiantes: ");
+            int estudiante=sc.nextInt();
+            for(int i = 1 ; i <= estudiante;i++){
+                System.out.print("nota del estudiante "+i+": ");
+                nota = sc.nextDouble();
+                if(nota>=5.0){
+                    aprobado++;
+                }else{
+                    reprobado++;
+                }
 
-        System.out.println("mi primer programa de java en github");
-        
-
-
-
-
+                
+            }
+            System.out.println("los aprobados son: "+aprobado);
+            System.out.println("los no aprobados son: "+reprobado);
     }
 }
