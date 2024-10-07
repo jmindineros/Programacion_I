df763b1710d29b07da69428abb08b65edebaca07
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-10-07 21:23:28 +0000

    trueAndFlase

--- Class07/EjmCond2.java
+++ Class07/EjmCond2.java
@@ -2,16 +2,9 @@ public class EjmCond2 {
 
     public static void main(String[] args) {
 
-        int a = 7 * 3 + 6 / 2 - 5;
-        int b = 21 - 8 + a % 3 * 11;
-        if (a < b) {
-            System.out.println("a is less than b");
-        }
-        if (a == b) {
-            System.out.println("a is equals to b");
-        }
-        if (a > b) {
-            System.out.println(" a is greather than b");
-        }
+if ( true && true && false ||false){System.out.println("true");
+    }else {
+        System.out.println("false");
+    }
     }
 }
