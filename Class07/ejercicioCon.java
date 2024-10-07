18084db997b1ace05b7d37854f0dae59a623df76
jmindineros <jmindineros@estudiante.uniajc.edu.co>
2024-10-07 20:03:21 +0000

    prueba_1

---
+++ Class07/ejercicioCon.java
@@ -0,0 +1,14 @@
+public class ejercicioCon {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        boolean c = true;
+        if(!a || (b&&c)){
+                System.out.println("sentencia if ejecutada");
+
+        }else{
+            System.out.println("sentencia else ejecutada");
+        }
+    }
+}
